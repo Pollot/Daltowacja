1423ef3234e78079bce05099b5379b5d455f84dd
Pollot <olaf.moroz.mirtex@gmail.com>
2023-09-24 01:27:55 +0200

    Fixed auto locale

--- app/src/main/java/com/daltowacja/daltowacja/languageManager.kt
+++ app/src/main/java/com/daltowacja/daltowacja/languageManager.kt
@@ -5,12 +5,25 @@ import android.content.res.Configuration
 import java.util.Locale
 
 object LanguageManager {
+    private var originalLocale: Locale? = null
+
     fun setAppLanguage(context: Context, selectedLanguage: String) {
-        if (selectedLanguage != "auto") {
-            val locale = Locale(selectedLanguage)
-            val config = Configuration(context.resources.configuration)
-            config.setLocale(locale)
-            context.resources.updateConfiguration(config, context.resources.displayMetrics)
+        if (originalLocale == null) {
+            // Store the original system locale when it's not already stored
+            originalLocale = context.resources.configuration.locales.get(0)
         }
+
+        val locale = if (selectedLanguage == "auto") {
+            // Use the original system locale for "auto"
+            originalLocale
+        } else {
+            // Use the selected language
+            Locale(selectedLanguage)
+        }
+
+        val config = Configuration(context.resources.configuration)
+        config.setLocale(locale)
+
+        context.resources.updateConfiguration(config, context.resources.displayMetrics)
     }
 }
