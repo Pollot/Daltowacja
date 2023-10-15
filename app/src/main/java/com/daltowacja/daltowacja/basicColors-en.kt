44fd2766a4b514317c34eb0b0379704c2428e4a0
Pollot <olaf.moroz.mirtex@gmail.com>
2023-10-15 03:21:02 +0200

    Basic colors updated

--- app/src/main/java/com/daltowacja/daltowacja/basicColors-en.kt
+++ app/src/main/java/com/daltowacja/daltowacja/basicColors-en.kt
@@ -2,18 +2,39 @@ package com.daltowacja.daltowacja
 
 val basicColors_en = mapOf(
     intArrayOf(255, 0, 0) to listOf("Red", "Intense color like red traffic lights"),
-    intArrayOf(255, 128, 0) to listOf("Orange", "Color between red and yellow"),
-    intArrayOf(255, 255, 0) to listOf("Yellow", "Bright color with a hue similar to sunlight"),
+    intArrayOf(255, 192, 192) to listOf("Light Red", "Pale shade of red"),
+    intArrayOf(139, 0, 0) to listOf("Dark Red", "Deep, dark shade of red"),
+
+    intArrayOf(0, 255, 0) to listOf("Green", "Color of grass and leaves"),
+    intArrayOf(144, 238, 144) to listOf("Light Green", "Pale shade of green"),
+    intArrayOf(0, 128, 0) to listOf("Dark Green", "Deep, dark shade of green"),
     intArrayOf(128, 255, 0) to listOf("Lime", "Intense shade of green, like a lime"),
-    intArrayOf(0, 255, 128) to listOf("Teal", "Color close to a turquoise shade, like sea water"),
-    intArrayOf(0, 255, 255) to listOf("Cyan", "Intense blue-green color"),
+
+    intArrayOf(0, 0, 255) to listOf("Blue", "Color resembling the sky on a clear day"),
     intArrayOf(0, 128, 255) to listOf("Light Blue", "Pale shade of blue resembling the sky"),
     intArrayOf(0, 0, 255) to listOf("Dark Blue", "Deep, dark shade of blue"),
+    intArrayOf(0, 255, 255) to listOf("Cyan", "Intense blue-green color"),
+    intArrayOf(0, 255, 128) to listOf("Teal", "Color close to a turquoise shade, like sea water"),
+
+    intArrayOf(255, 128, 0) to listOf("Orange", "Color between red and yellow"),
+    intArrayOf(255, 165, 0) to listOf("Light Orange", "Pale shade of orange"),
+    intArrayOf(255, 140, 0) to listOf("Dark Orange", "Deep, dark shade of orange"),
+
+    intArrayOf(255, 255, 0) to listOf("Yellow", "Bright color with a hue similar to sunlight"),
+    intArrayOf(255, 255, 128) to listOf("Light Yellow", "Pale shade of yellow"),
+    intArrayOf(184, 134, 11) to listOf("Dark Yellow", "Deep, dark shade of yellow"),
+
     intArrayOf(128, 0, 255) to listOf("Purple", "Color between blue and red"),
     intArrayOf(255, 0, 255) to listOf("Magenta", "Intense color with shades of red and blue"),
     intArrayOf(255, 0, 128) to listOf("Fuchsia", "Pale shade of pink, like fuchsia flowers"),
+
     intArrayOf(0, 0, 0) to listOf("Black", "Lack of color or light reflection, darkest shade"),
+    intArrayOf(139, 69, 19) to listOf("Brown", "Natural color resembling wood tone"),
+    intArrayOf(222, 184, 135) to listOf("Light Brown", "Pale shade of brown"),
+    intArrayOf(139, 69, 19) to listOf("Dark Brown", "Deep, dark shade of brown"),
+
     intArrayOf(255, 255, 255) to listOf("White", "Color reflecting all light wavelengths"),
     intArrayOf(128, 128, 128) to listOf("Gray", "Color between white and black"),
-    intArrayOf(139, 69, 19) to listOf("Brown", "Natural color resembling wood tone")
+    intArrayOf(211, 211, 211) to listOf("Light Gray", "Pale shade of gray"),
+    intArrayOf(169, 169, 169) to listOf("Dark Gray", "Deep, dark shade of gray")
 )
--- app/src/main/java/com/daltowacja/daltowacja/basicColors-pl.kt
+++ app/src/main/java/com/daltowacja/daltowacja/basicColors-pl.kt
@@ -1,19 +1,40 @@
 package com.daltowacja.daltowacja
 
 val basicColors_pl = mapOf(
-    intArrayOf(255, 0, 0) to listOf("Czerwony", "Intensywny kolor jak czerwone latarnie na sygnalizatorach drogowych"),
-    intArrayOf(255, 128, 0) to listOf("Pomarańczowy", "Kolor pomiędzy czerwonym a żółtym"),
-    intArrayOf(255, 255, 0) to listOf("Żółty", "Jasny kolor o barwie podobnej do słonecznego światła"),
+    intArrayOf(255, 0, 0) to listOf("Czerwony", "Intensywny kolor jak czerwone światło sygnalizacji drogowej"),
+    intArrayOf(255, 192, 192) to listOf("Jasnoczerwony", "Blada odcień czerwieni"),
+    intArrayOf(139, 0, 0) to listOf("Ciemnoczerwony", "Głęboki, ciemny odcień czerwieni"),
+
+    intArrayOf(0, 255, 0) to listOf("Zielony", "Kolor trawy i liści"),
+    intArrayOf(144, 238, 144) to listOf("Jasnozielony", "Blady odcień zieleni"),
+    intArrayOf(0, 128, 0) to listOf("Ciemnozielony", "Głęboki, ciemny odcień zieleni"),
     intArrayOf(128, 255, 0) to listOf("Limonkowy", "Intensywny odcień zieleni, jak limonka"),
-    intArrayOf(0, 255, 128) to listOf("Morski", "Kolor zbliżony do odcienia turkusowego, jak morska woda"),
-    intArrayOf(0, 255, 255) to listOf("Cyjan", "Intensywny kolor niebiesko-zielony"),
-    intArrayOf(0, 128, 255) to listOf("Jasny niebieski", "Jasny odcień koloru niebieskiego, przypominający błękitne niebo"),
-    intArrayOf(0, 0, 255) to listOf("Ciemny niebieski", "Głęboki, ciemny kolor odcienia niebieskiego"),
-    intArrayOf(128, 0, 255) to listOf("Fioletowy", "Kolor pomiędzy niebieskim a czerwonym"),
-    intArrayOf(255, 0, 255) to listOf("Magenta", "Intensywny kolor odcienia czerwonego i niebieskiego"),
-    intArrayOf(255, 0, 128) to listOf("Fuksja", "Jasny odcień różowego, jak kwiaty fuksji"),
-    intArrayOf(0, 0, 0) to listOf("Czarny", "Brak barwy lub odbicia światła, najciemniejszy kolor"),
-    intArrayOf(255, 255, 255) to listOf("Biały", "Kolor odbijający wszystkie barwy światła"),
-    intArrayOf(128, 128, 128) to listOf("Szary", "Kolor pomiędzy białym a czarnym"),
-    intArrayOf(139, 69, 19) to listOf("Brązowy", "Naturalny kolor zbliżony do barwy drewna")
+
+    intArrayOf(0, 0, 255) to listOf("Niebieski", "Kolor przypominający niebo w bezchmurny dzień"),
+    intArrayOf(0, 128, 255) to listOf("Jasnoniebieski", "Blady odcień niebieskiego przypominający niebo"),
+    intArrayOf(0, 0, 255) to listOf("Ciemnoniebieski", "Głęboki, ciemny odcień niebieskiego"),
+    intArrayOf(0, 255, 255) to listOf("Błękitny", "Intensywny kolor niebiesko-zielony"),
+    intArrayOf(0, 255, 128) to listOf("Turkusowy", "Kolor zbliżony do odcienia turkusu, jak woda morska"),
+
+    intArrayOf(255, 128, 0) to listOf("Pomarańczowy", "Kolor między czerwonym a żółtym"),
+    intArrayOf(255, 165, 0) to listOf("Jasnopomarańczowy", "Blady odcień pomarańczy"),
+    intArrayOf(255, 140, 0) to listOf("Ciemnopomarańczowy", "Głęboki, ciemny odcień pomarańczy"),
+
+    intArrayOf(255, 255, 0) to listOf("Żółty", "Jasny kolor o odcieniu podobnym do światła słonecznego"),
+    intArrayOf(255, 255, 128) to listOf("Jasnożółty", "Blady odcień żółtego"),
+    intArrayOf(184, 134, 11) to listOf("Ciemnożółty", "Głęboki, ciemny odcień żółtego"),
+
+    intArrayOf(128, 0, 255) to listOf("Purpurowy", "Kolor między niebieskim a czerwonym"),
+    intArrayOf(255, 0, 255) to listOf("Purpuroworóżowy", "Intensywny kolor o odcieniach czerwieni i niebieskiego"),
+    intArrayOf(255, 0, 128) to listOf("Fuksja", "Blady odcień różowego, jak kwiaty fuksji"),
+
+    intArrayOf(0, 0, 0) to listOf("Czarny", "Brak koloru lub odbicia światła, najciemniejszy odcień"),
+    intArrayOf(139, 69, 19) to listOf("Brązowy", "Naturalny kolor przypominający odcień drewna"),
+    intArrayOf(222, 184, 135) to listOf("Jasnobrązowy", "Blady odcień brązu"),
+    intArrayOf(139, 69, 19) to listOf("Ciemnobrązowy", "Głęboki, ciemny odcień brązu"),
+
+    intArrayOf(255, 255, 255) to listOf("Biały", "Kolor odbijający wszystkie długości fali świetlnej"),
+    intArrayOf(128, 128, 128) to listOf("Szary", "Kolor między białym a czarnym"),
+    intArrayOf(211, 211, 211) to listOf("Jasnoszary", "Blady odcień szarości"),
+    intArrayOf(169, 169, 169) to listOf("Ciemnoszary", "Głęboki, ciemny odcień szarości")
 )
